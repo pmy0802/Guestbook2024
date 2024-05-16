41f26665868a86e50829956dfd0c92d6f5887920
pmy0802 <pmy0802@naver.com>
2024-05-16 11:49:07 +0900

    서비스계층 추가 및 목록처리에 필요한 정보 저장, 테스트

--- src/main/java/kr/ac/kopo/guestbook2024/service/GuestbookService.java
+++ src/main/java/kr/ac/kopo/guestbook2024/service/GuestbookService.java
@@ -1,10 +1,15 @@
 package kr.ac.kopo.guestbook2024.service;
 
 import kr.ac.kopo.guestbook2024.dto.GuestbookDTO;
+import kr.ac.kopo.guestbook2024.dto.PageRequestDTO;
+import kr.ac.kopo.guestbook2024.dto.PageResultDTO;
 import kr.ac.kopo.guestbook2024.entity.Guestbook;
 
 public interface GuestbookService {
+    // 글등록 기능
     Long register(GuestbookDTO dto);
+    // 한페이지에 보여질 글 목록(GuestbookDTO 객체)이 저장된 List정보를 갖고 있는 PageResultDTO객체 참조값을 반환하는 기능
+    PageResultDTO<GuestbookDTO, Guestbook> getList(PageRequestDTO requestDTO);
 
     default Guestbook dtoToEntity(GuestbookDTO dto){
         Guestbook entity = Guestbook.builder()
@@ -13,7 +18,21 @@ public interface GuestbookService {
                 .content(dto.getContent())
                 .writer(dto.getWriter())
                 .build();
+
         return entity;
+    }
+
+    default GuestbookDTO entityToDto(Guestbook entity){
+        GuestbookDTO dto = GuestbookDTO.builder()
+                .gno(entity.getGno())
+                .title(entity.getTitle())
+                .content(entity.getContent())
+                .writer(entity.getWriter())
+                .regDate(entity.getRegDate())
+                .modDate(entity.getModDate())
+                .build();
+
+        return dto;
     }
 
 
